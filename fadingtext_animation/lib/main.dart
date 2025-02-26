bd0f6106620c735482580697a11d69652162c763
Madhuri Tumula <madhuritumula@gmail.com>
2025-02-26 13:44:06 -0500

    Toggle between Day mode and Night mode

--- fadingtext_animation/lib/main.dart
+++ fadingtext_animation/lib/main.dart
@@ -1,103 +1,120 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 void main() {
-
-runApp(MyApp());
-
+  runApp(MyApp());
 }
 
-
-
-class MyApp extends StatelessWidget {
-
-@override
-
-Widget build(BuildContext context) {
-
-return MaterialApp(
-
-home: FadingTextAnimation(),
-
-);
-
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
+class _MyAppState extends State<MyApp> {
+  ThemeData _lightTheme = ThemeData.light();
+  ThemeData _darkTheme = ThemeData.dark();
+  bool _isDarkMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: _isDarkMode ? _darkTheme : _lightTheme,
+      home: FadingTextScreen(toggleTheme: _toggleTheme),
+    );
+  }
+
+  void _toggleTheme() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
 }
 
+class FadingTextScreen extends StatefulWidget {
+  final VoidCallback toggleTheme;
 
+  FadingTextScreen({required this.toggleTheme});
 
-class FadingTextAnimation extends StatefulWidget {
-
-@override
-
-_FadingTextAnimationState createState() => _FadingTextAnimationState();
-
+  @override
+  _FadingTextScreenState createState() => _FadingTextScreenState();
 }
 
+class _FadingTextScreenState extends State<FadingTextScreen> {
+  bool _isVisible = true;
+  Color _textColor = Colors.black;
 
+  void _toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
 
-class _FadingTextAnimationState extends State<FadingTextAnimation> {
+  void _changeTextColor() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Pick a text color"),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              pickerColor: _textColor,
+              onColorChanged: (color) {
+                setState(() {
+                  _textColor = color;
+                });
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text("Done"),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+        );
+      },
+    );
+  }
 
-bool _isVisible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fading Text Animation'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.brightness_6),
+            onPressed: widget.toggleTheme,
+          ),
+          IconButton(
+            icon: Icon(Icons.color_lens),
+            onPressed: _changeTextColor,
+          ),
+        ],
+      ),
+      body: PageView(
+        children: [
+          _buildFadingTextPage(Duration(seconds: 1)),
+          _buildFadingTextPage(Duration(seconds: 3)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleVisibility,
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
 
-
-
-void toggleVisibility() {
-
-setState(() {
-
-_isVisible = !_isVisible;
-
-});
-
-}
-
-
-
-@override
-
-Widget build(BuildContext context) {
-
-return Scaffold(
-
-appBar: AppBar(
-
-title: Text('Fading Text Animation'),
-
-),
-
-body: Center(
-
-child: AnimatedOpacity(
-
-opacity: _isVisible ? 1.0 : 0.0,
-
-duration: Duration(seconds: 1),
-
-child: Text(
-
-'Hello, Flutter!',
-
-style: TextStyle(fontSize: 24),
-
-),
-
-),
-
-),
-
-floatingActionButton: FloatingActionButton(
-
-onPressed: toggleVisibility,
-
-child: Icon(Icons.play_arrow),
-
-),
-
-);
-
-}
-
+  Widget _buildFadingTextPage(Duration duration) {
+    return Center(
+      child: AnimatedOpacity(
+        opacity: _isVisible ? 1.0 : 0.0,
+        duration: duration,
+        child: Text(
+          'Hello, Flutter!',
+          style: TextStyle(fontSize: 24, color: _textColor),
+        ),
+      ),
+    );
+  }
 }
